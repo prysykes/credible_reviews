9bffd39461789ba94a928dc084d7e0efd0a96f1e
Chukwuemeka Duru <70113615+prysykes@users.noreply.github.com>
2024-05-31 22:15:58 -0400

    Update settings.py

--- credible_reviews/settings.py
+++ credible_reviews/settings.py
@@ -151,5 +151,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'crediblereviewsng@gmail.com'
-EMAIL_HOST_PASSWORD = 'Emekamu123@'
